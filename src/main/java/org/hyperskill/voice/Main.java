ab53cf53a7823b01b8547263e43c0fa23c998ce9
Alina <alina.parfenteva@mail.ru>
2018-12-28 21:51:25 +0300

    fill main class

--- src/main/java/org/hyperskill/voice/Main.java
+++ src/main/java/org/hyperskill/voice/Main.java
@@ -1,7 +1,25 @@
 package org.hyperskill.voice;
-
+import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.LiveSpeechRecognizer;
+import edu.cmu.sphinx.api.SpeechResult;
+import edu.cmu.sphinx.result.WordResult;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws Exception {
+        Configuration configuration = new Configuration();
+        configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
+        configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
+        configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
+        configuration.setUseGrammar(false);
+
+        LiveSpeechRecognizer recognizer = new LiveSpeechRecognizer(configuration);
+        System.out.println("Записывает");
+        recognizer.startRecognition(true);
+
+        SpeechResult result;
+        result=recognizer.getResult();
+        String word=result.getHypothesis();
+        System.out.println("your word:");
+        System.out.println(word);
+        recognizer.stopRecognition();
     }
 }
