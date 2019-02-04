23359866736d2a226b985df13f689b448715e6e4
Kirill <aspushkin2395@gmail.com>
2019-02-05 00:06:15 +0300

    Code added

--- src/main/java/org/hyperskill/voice/Main.java
+++ src/main/java/org/hyperskill/voice/Main.java
@@ -1,7 +1,26 @@
 package org.hyperskill.voice;
 
+import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.LiveSpeechRecognizer;
+import edu.cmu.sphinx.api.SpeechResult;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello World!");
+        Configuration conf = new Configuration();
+        conf.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
+//        conf.setAcousticModelPath("resource:/edu/cmu/sphinx/models/ru-ru/ru-ru");
+        conf.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
+//        conf.setDictionaryPath("resource:/edu/cmu/sphinx/models/ru-ru/cmudict-ru-ru.dict");
+        conf.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
+//        conf.setLanguageModelPath("resource:/edu/cmu/sphinx/models/ru-ru/ru-ru.lm.bin");
+
+        LiveSpeechRecognizer lsr = new LiveSpeechRecognizer(conf);
+        lsr.startRecognition(true);
+        SpeechResult speechResult = lsr.getResult();
+        lsr.stopRecognition();
+        System.out.println(speechResult.getHypothesis());
     }
 }
